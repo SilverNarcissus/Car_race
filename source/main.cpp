bd63ff4adaf74945a33663427876a136e7ae0236
151250176 <151250176@smail.nju.edu.cn>
2017-09-07 17:25:16 +0800

    add include and lib for google_test and mysql

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,21 @@
-#include "../test/my_test.h"
-#include <iostream>
 
-int main() {
-    std::cout << test1();
-    return 0;
+//
+// Created by Silver Narcissus on 2017/9/7.
+//
+#include <iostream>
+#include "../googletest/include/gtest/gtest.h"
+#include "../test/my_test.h"
+
+TEST(database__Test,test1) {
+    EXPECT_EQ(-1, test1());
 }
 
+GTEST_API_ int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
 
+//int main(){
+//    std::cout<<"Hello World!"<<std::endl;
+//    return 0;
+//}
